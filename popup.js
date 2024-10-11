4b912f52c8c7d67a8f00b67dd905e0504929dbd9
Leila <soltani.leil@gmail.com>
2024-10-10 20:34:47 -0500

    some bugs fixed

--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('activateButton').addEventListener('click', () => {
       // Get the active tab
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         let activeTab = tabs[0].id;
   
-        // Inject content.js into the active tab
+        // Inject content.js into the active tab only when the button is clicked
         chrome.scripting.executeScript({
           target: {tabId: activeTab},
           files: ['content.js']
